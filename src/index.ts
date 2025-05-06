086a5e8f19a57e2bc76be76f345cd1c9ed5ad46e
Chris Earle <pickypg@gmail.com>
2025-05-05 21:55:38 -0600

    Add /help API for 2.0

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,13 @@ app.all('/', (_req, res) => {
   });
 });
 
+app.all('/help', (_req, res) => {
+  res.json({
+    message: `This is some help text for ${version}`,
+    version,
+  });
+});
+
 app.listen(1234, () => {
   console.log(`Server running on port 1234`);
 });
